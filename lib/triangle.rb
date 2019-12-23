f6e6bf8daf20f1d37a90195da168150bfac6994c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-12-23 20:30:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(attribute)
+    attribute.each {|key, value}| self.send(("#{key}="), value)
+  end
+
+  def kind
+    if side_1 = side_2 != {side_3}
+      return "isosceles"
+    elsif side_1 = side_2 = side_3
+      return "equilateral"
+    else
+      return "scalene"
+      
+      
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
+  
 end
